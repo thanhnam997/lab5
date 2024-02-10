14391fdeca7349ef94366ed80bea315b16661ae2
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-10 16:29:45 -0600

    Add files via upload

---
+++ javascripliterals.js
@@ -0,0 +1,20 @@
+const name = 'Jack';
+const greet = true;
+
+function tagExample(strings, nameValue) {
+    let str0 = strings[0]; // Hello
+    let str1 = strings[1]; // , How are you?
+
+    if(greet) {
+        return `${str0}${nameValue}${str1}`;
+    }
+}
+
+// creating tagged literal
+// passing argument name
+const result = tagExample`Hello ${name}, How are you?`;
+
+console.log(result);
+
+
+
