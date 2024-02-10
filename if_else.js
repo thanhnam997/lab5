82a7531c5337a5af6e204293d0381aa966b04a2f
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-10 17:00:19 -0600

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,30 @@
+// JavaScript conditional if...else
+
+let a = 5;
+let b = 10;
+
+// Case 1
+if (a < b) {
+    console.log('condition a < b');
+}
+
+// Case 2
+if (a > b) {
+    console.log('condition a > b');
+}
+
+// Case 3
+if (a > b) {
+    console.log('condition a > b');
+} else {
+    console.log('condition a > b is not met');
+}
+
+// Case 4
+if (a > b) {
+    console.log('condition a > b');
+} else if (a < b) {
+    console.log('condition a > b is not met but condition a < b');
+} else {
+    console.log('the above condition is not met');
+}
