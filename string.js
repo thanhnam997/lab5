65c75f6af8114f8f0880f71279baba28689f265d
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-10 17:55:40 -0600

    Add files via upload

---
+++ string.js
@@ -0,0 +1,10 @@
+let greeting = 'welcome to nodemy,Nodemy is stand for Academy';
+
+// Split words
+let wordsArray = greeting.split(' ');
+
+// Replace 'nodemy' with 'NODEMY'
+let modifiedGreeting = greeting.replace('nodemy', 'NODEMY');
+
+console.log('Words array:', wordsArray);
+console.log('Modified greeting:', modifiedGreeting);
