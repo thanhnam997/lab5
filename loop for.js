c55583f01155a074937796b1557eae9337d12482
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-10 17:31:34 -0600

    Add files via upload

---
+++ loop for.js
@@ -0,0 +1,11 @@
+// calculate the sum of the first 50 natural numbers
+let sum = 0;
+const n = 50;
+// each iteration i is increased by 1
+// loop from i = 1 to 50 i.e. repeat 50 times
+
+for (let i = 1; i <= n; i++) {
+    sum += i;     // sum = sum + i
+}
+
+  console.log('sum:', sum);
