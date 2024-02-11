e7a48d90254fc834c25a3174b7210b4e01fe3105
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-10 18:15:49 -0600

    Add files via upload

---
+++ object.js
@@ -0,0 +1,11 @@
+// Define a Car object
+let myCar = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2022,
+};
+
+// Accessing properties
+console.log(myCar.make);  // Output: Toyota
+console.log(myCar.model); // Output: Camry
+console.log(myCar.year);  // Output: 2022
