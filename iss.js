8365594cbe46bb443c85437f12b3cc4dece55b44
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-21 18:08:22 -0600

    Add files via upload

---
+++ iss.js
@@ -0,0 +1,23 @@
+/* Lab: write the code requested by lines marked //TODO
+You should NOT modify any of the code that's here already. Add the code requested.   */
+
+console.log('For this lab, please write the code requested at the //TODO markers.')
+
+/* a. Use this JavaScript object. This represents the current position of the International Space Station
+at 1pm on January 12th 2018, fetched from http://api.open-notify.org/iss-now.json.
+ */
+
+let iss_location = {
+  "timestamp": 1515784140,
+  "iss_position":
+    {
+      "latitude": "49.2167",
+      "longitude": "100.5363"
+    },
+  "message": "success"
+}
+
+// TODO Extract the latitude value, and log it to the console.
+console.log("Latitude:", iss_location.iss_position.latitude);
+// TODO Extract the longitude value, and log it to the console.
+console.log("Longitude:", iss_location.iss_position.longitude);
